51075c950108bde53399575d47794e5420cd221e
Thiago Feijó <tfeijo@gmail.com>
2021-03-24 15:39:48 -0300

    Index and show farm adjusts

--- src/controllers/FarmController.py
+++ src/controllers/FarmController.py
@@ -3,19 +3,33 @@ from owlready2 import World, OwlReadyError, sync_reasoner_pellet
 from src.models.Classes import farm_to_json
 from src.ontology.config import increase_id, decrease_id
 from src.utils.methods import *
+import glob
 
 
 class FarmController():
   
   def index():
-    default_world = World(filename = "./src/ontology/db.sqlite3", exclusive=False)
-    onto = default_world.get_ontology("./src/ontology/db.owl").load()
-    
-    query_farm = onto.Farm.instances()
     farms = []
-    for farm in query_farm:
-      farms.append(farm_to_json(farm))
-    
+    for file in glob.iglob('./src/ontology/temp/*.owl', recursive = True):
+      file = file[:-4]
+      
+      try:
+        default_world = World(filename = f'{file}.sqlite3', exclusive=False)
+        onto = default_world.get_ontology(f'{file}.owl').load()
+        query_farm = onto.Farm.instances()
+      
+        for farm in query_farm:
+          farms.append(farm_to_json(farm))
+        
+      except OwlReadyError as e:
+        print(f'Something went wrong in query: {e}')
+        return jsonify({"Error": "Something went wrong in query"}), 400
+        
+      finally:
+        onto.destroy()
+        default_world.close()
+      
+      
     return jsonify(farms)
 
   def store(farm):
@@ -66,7 +80,20 @@ class FarmController():
       
 
   def show(id):
-    farm = onto.search_one(is_a=Farm, id=id)
-    if farm == None: return jsonify({ 'error': 'Not found'}), 404
-    json = farm.to_json()
-    return jsonify(json)
+    
+    file = f'./src/ontology/temp/{id}'
+    
+    try:
+      default_world = World(filename = f'{file}.sqlite3', exclusive=False)
+      onto = default_world.get_ontology(f'{file}.owl').load()
+      farm = farm_to_json(onto.search_one(is_a=onto.Farm, id=id))
+      
+    except OwlReadyError as e:
+      print(f'Something went wrong in query: {e}')
+      return jsonify({"Error": "Something went wrong in query"}), 400
+      
+    finally:
+      onto.destroy()
+      default_world.close()
+      
+    return jsonify(farm)
